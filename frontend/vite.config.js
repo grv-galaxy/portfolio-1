1039f12dac77da61112bdb7a3f2f5a419e225c19
Gaurav Kumar <24f1000758@ds.study.iitm.ac.in>
2026-06-22 15:02:53 +0530

    Initital commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/portfolio-1/',   // ← add this line
-  plugins: [vue()]
-<<<<<<< HEAD
+  plugins: [vue()],
+  base: '/portfolio-1/',
 })
-=======
-})
->>>>>>> 4a5cf7ddfc21c50951d6b0bb597518cbb7bdae51
